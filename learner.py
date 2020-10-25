c6288b0859a13d770420fb926301595fca7f8bdc
risto <ristohinno@gmail.com>
2020-10-25 19:13:28 +0200

    refactored training into subfunctions, changed plotting, added evaluation based on subset of original data

--- learner.py
+++ learner.py
@@ -13,7 +13,7 @@ import torch.optim as optim
 from src.utils import load_pretrained_embeddings
 from src.qt_model import QuickThoughts
 from src.utils import checkpoint_training, restore_training, safe_pack_sequence, VisdomLinePlotter
-from src.data.corpus import Corpus
+from src.data.corpus import Corpus, create_train_eval_corpus
 from src.eval import test_performance
 
 _LOGGER = logging.getLogger(__name__)
@@ -53,10 +53,58 @@ class QTLearner:
         _LOGGER.info(pformat(self.gather_conf_info()))
         _LOGGER.info(f"Wrote config to file: {self.config_filepath}")
 
-    def fit_epoch(self, train_iter, failed_or_skipped_batches, optimizer, qt, plotter):
-        temp = tqdm(train_iter)
+    def forward_pass(self, qt, data, mode='train'):
+        # forward pass
+        if mode == 'train':
+            enc_f, enc_g = qt(data)
+        else: #in eval mode returns concatenated enc_f and enc_g
+            enc = qt(data)
+            enc_f, enc_g = enc[:, :self.hidden_size], enc[:, self.hidden_size:]
 
-        for i, data in enumerate(temp):
+        # calculate scores
+        scores = torch.matmul(enc_f, enc_g.t())
+
+        # zero out when it's the same sentence
+        if torch.cuda.is_available():
+            mask = torch.eye(len(scores)).cuda().bool()
+        else:
+            mask = torch.eye(len(scores)).bool()
+        scores.masked_fill_(mask, 0)
+
+        # return log scores and target
+        block_log_scores = F.log_softmax(scores, dim=1)
+        # targets also topelitz matrix
+        targets = qt.generate_targets(self.batch_size, offsetlist=[1])
+        loss = self.kl_loss(block_log_scores, targets)
+        return loss
+
+    def fit_batch(self, qt, data, optimizer, mode='train'):
+        loss = self.forward_pass(qt, data, mode)
+        if mode == 'train':
+            loss.backward()
+            # grad clipping
+            nn.utils.clip_grad_norm_(filter(lambda p: p.requires_grad, qt.parameters()), self.norm_threshold)
+            optimizer.step()
+        return loss
+
+    def fit_eval_epoch(self, train_data_iter, eval_data_iter, qt, optimizer, failed_or_skipped_batches, plotter):
+        loss_train, failed_or_skipped_batches_train = self.fit_epoch(train_data_iter, failed_or_skipped_batches,
+                                                                     optimizer, qt, plotter, mode='train')
+        failed_or_skipped_batches += failed_or_skipped_batches_train
+        loss_eval, _ = self.fit_epoch(eval_data_iter, failed_or_skipped_batches, optimizer, qt, plotter,
+                                      mode='eval')
+
+        return loss_train, loss_eval, failed_or_skipped_batches
+
+    def fit_epoch(self, data_iter, failed_or_skipped_batches, optimizer, qt, plotter, mode='train'):
+        if mode == 'train':
+            qt.train()
+        else:
+            qt.eval()
+
+        data_iter_tq = tqdm(data_iter)
+        loss = 0
+        for i, data in enumerate(data_iter_tq):
             # deal with bad sequences
             if not data:
                 failed_or_skipped_batches += 1
@@ -70,58 +118,45 @@ class QTLearner:
                 if torch.cuda.is_available():
                     data = data.cuda()
 
-                # forward pass
-                enc_f, enc_g = qt(data)
+                loss = self.fit_batch(qt, data, optimizer, mode)
 
-                # calculate scores
-                scores = torch.matmul(enc_f, enc_g.t())
-                # zero out when it's the same sentence
-                if torch.cuda.is_available():
-                    mask = torch.eye(len(scores)).cuda().bool()
-                else:
-                    mask = torch.eye(len(scores)).bool()
-                scores.masked_fill_(mask, 0)
-
-                # return log scores and target
-                block_log_scores = F.log_softmax(scores, dim=1)
-                # targets also topelitz matrix
-                targets = qt.generate_targets(self.batch_size, offsetlist=[1])
-                loss = self.kl_loss(block_log_scores, targets)
-
-                loss.backward()
-                # grad clipping
-                nn.utils.clip_grad_norm_(filter(lambda p: p.requires_grad, qt.parameters()), self.norm_threshold)
-                optimizer.step()
-
-                temp.set_description("loss {:.4f} | failed/skipped {:3d}".format(loss, failed_or_skipped_batches))
-
-                if i % 1 == 0:
-                    plotter.plot('loss', 'train', 'Run: {} Loss'.format(str(self.checkpoint_dir).split('/')[-1]), i,
-                                 loss.item())
+                data_iter_tq.set_description(
+                    "loss {} {:.4f} | failed/skipped {:3d}".format(mode, loss, failed_or_skipped_batches))
 
             except Exception as e:
                 _LOGGER.exception(e)
                 failed_or_skipped_batches += 1
                 if torch.cuda.is_available():
                     torch.cuda.empty_cache()
+        return loss, failed_or_skipped_batches
 
-    def eval(self, i, qt, optimizer, vocab, plotter):
-        checkpoint_training(self.checkpoint_dir, i, qt, optimizer)
+    def eval_downstream(self, qt, vocab, datasets=['MR']):
         qt.eval()
-        for dataset in ['MR']:
+        accs = []
+        for dataset in datasets:
             acc = test_performance(qt, vocab, dataset, 'data', seed=int(time.time()))
-            plotter.plot('acc', dataset, 'Downstream Accuracy', i, acc, xlabel='seconds')
-        qt.train()
+            accs.append(acc)
+        return accs
+        #     plotter.plot('acc', dataset, 'Downstream Accuracy', i, acc, xlabel='seconds')
+        # qt.train()
 
-    def create_dataloader(self, pretrained_embeddings):
-        bookcorpus = Corpus(self.data_path, pretrained_embeddings.vocab)
-        train_iter = DataLoader(bookcorpus,
+    def create_dataloaders(self, pretrained_embeddings, eval_p=0.2):
+        bookcorpus_train, bookcorpus_eval = create_train_eval_corpus(self.data_path, pretrained_embeddings.vocab,
+                                                                     eval_p)
+        train_iter = DataLoader(bookcorpus_train,
                                 batch_size=self.batch_size,
                                 num_workers=1,
                                 drop_last=True,
                                 pin_memory=True,  # send to GPU
                                 collate_fn=safe_pack_sequence)
-        return train_iter
+
+        eval_iter = DataLoader(bookcorpus_eval,
+                               batch_size=self.batch_size,
+                               num_workers=1,
+                               drop_last=True,
+                               pin_memory=True,  # send to GPU
+                               collate_fn=safe_pack_sequence)
+        return train_iter, eval_iter
 
     def fit(self):
         self._init_logging()
@@ -130,8 +165,8 @@ class QTLearner:
         # load in word vectors
         WV_MODEL = load_pretrained_embeddings(self.embedding)
 
-        # create dataset
-        train_iter = self.create_dataloader(WV_MODEL)
+        # create datasets
+        train_iter, eval_iter = self.create_dataloaders(WV_MODEL)
 
         # model, optimizer, and loss function
         qt = QuickThoughts(WV_MODEL, self.hidden_size)  # .cuda()
@@ -144,10 +179,24 @@ class QTLearner:
         # last_train_idx = restore_training(self.checkpoint_dir, qt, optimizer) if self.resume else -1
         # start = time.time()
         # block_size = 5
-
+        best_eval_loss = None
         for j in range(self.num_epochs):
-            self.fit_epoch(train_iter, failed_or_skipped_batches, optimizer, qt, plotter)
-            self.eval(j, qt, optimizer, WV_MODEL.vocab, plotter)
+            loss_train, loss_eval, failed_or_skipped_batches = self.fit_eval_epoch(train_iter, eval_iter, qt, optimizer,
+                                                                                   failed_or_skipped_batches, plotter)
+            downstream_datasets = ['MR']
+            downstream_accs = self.eval_downstream(qt, WV_MODEL.vocab, datasets=downstream_datasets)
+            if best_eval_loss is None or best_eval_loss > loss_eval:
+                best_eval_loss = loss_eval
+                checkpoint_training(self.checkpoint_dir, j, qt, optimizer)
+
+            plotter.plot('loss', 'train', 'Loss train', j, loss_train.item(), xlabel='epoch')
+            plotter.plot('loss', 'eval', 'Loss eval', j, loss_eval.item(), xlabel='epoch')
+            for i, acc in enumerate(downstream_accs):
+                plotter.plot('acc', f'accuracy {downstream_datasets[i]}', 'Downstream accuracy', j, downstream_accs[i],
+                             xlabel='epoch')
+
+            # self.fit_epoch(train_iter, failed_or_skipped_batches, optimizer, qt, plotter)
+            # self.eval(eval_iter, j, qt, optimizer, WV_MODEL.vocab, plotter)
 
     def check_conf(self):
         pass
--- src/data/corpus.py
+++ src/data/corpus.py
@@ -4,16 +4,30 @@ from torch.utils.data.dataset import Dataset
 from src.data.utils import prepare_sequence
 
 _LOGGER = logging.getLogger(__name__)
+
+
 class Corpus(Dataset):
 
-    def __init__(self, file_path, vocab,  max_len=50):
+    def __init__(self, examples, vocab, max_len=50):
         self.vocab = vocab
-        with open(file_path) as f:
-            self.examples = list(f)
-        _LOGGER.info("Successfully read {} lines from file: {}".format(len(self.examples), file_path))
+        self.examples = examples
+        # with open(file_path) as f:
+        #   self.examples = list(f)
+
+    # _LOGGER.info("Successfully read {} lines from file: {}".format(len(self.examples), file_path))
 
     def __getitem__(self, i):
         return torch.LongTensor(prepare_sequence(self.examples[i], self.vocab))
 
     def __len__(self):
         return len(self.examples)
+
+
+def create_train_eval_corpus(file_path, vocab, eval_p=0.2):
+    with open(file_path) as f:
+        examples = list(f)
+        _LOGGER.info("Successfully read {} lines from file: {}".format(len(examples), file_path))
+    eval_start_i=int(len(examples)*(1-eval_p))
+    train_corpus = Corpus(examples[:eval_start_i], vocab)
+    eval_corpus = Corpus(examples[eval_start_i:], vocab)
+    return train_corpus, eval_corpus
