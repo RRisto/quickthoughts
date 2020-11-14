8ab77a7da49966e82f53b597d8d220edad037d40
risto <ristohinno@gmail.com>
2020-11-14 15:51:15 +0200

    updated conf

--- train_dev.py
+++ train_dev.py
@@ -6,5 +6,5 @@ from src.learner import QTLearner
 if __name__ == '__main__':
     if os.path.isdir('checkpoints/dev'):
         shutil.rmtree('checkpoints/dev')
-    learner = QTLearner.create_from_conf('src/config_dev.py')
+    learner = QTLearner.create_from_conf('conf/config_dev.py')
     learner.fit(plot=True)
