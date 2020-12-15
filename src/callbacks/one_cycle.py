5d04980756759f88d869b1a5327c33bcfb71cd9d
risto <ristohinno@gmail.com>
2020-12-15 20:48:38 +0200

    fix one_cycle

--- src/callbacks/one_cycle.py
+++ src/callbacks/one_cycle.py
@@ -10,20 +10,19 @@ class OneCycle(Callback):
         return True
 
     def begin_batch(self, data):
-        if not self.handler.in_train:
-            return
-        self.num += 1
-        n = len(self.learn.train_iter)
-        bn = self.epoch * n + self.num
-        mn = self.learn.num_epochs * n
-        pct = bn / mn
-        pct_start, div_start = 0.25, 10
-        if pct < pct_start:
-            pct /= pct_start
-            lr = (1 - pct) * self.base_lr / div_start + pct * self.base_lr
-        else:
-            pct = (pct - pct_start) / (1 - pct_start)
-            lr = (1 - pct) * self.base_lr
-        self.learn.opt.lr = lr
-        self.lrs.append(lr)
+        if self.handler.in_train:
+            self.num += 1
+            n = len(self.learn.train_iter)
+            bn = self.epoch * n + self.num
+            mn = self.learn.num_epochs * n
+            pct = bn / mn
+            pct_start, div_start = 0.25, 10
+            if pct < pct_start:
+                pct /= pct_start
+                lr = (1 - pct) * self.base_lr / div_start + pct * self.base_lr
+            else:
+                pct = (pct - pct_start) / (1 - pct_start)
+                lr = (1 - pct) * self.base_lr
+            self.learn.opt.lr = lr
+            self.lrs.append(lr)
         return True
